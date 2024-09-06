ec0c8860c3885332272255b723f8a2e5fc883c59
meit <zhora.makarov.2005@yandex.ru>
2024-09-06 17:40:42 +0900

    gg

--- server/server.js
+++ server/server.js
@@ -102,142 +102,30 @@ app.post("/update-image", (req, res) => {
 app.post("/update-text", (req, res) => {
   const changes = req.body.changes;
 
-  console.log("Получен запрос на обновление текста:", changes);
+  const dbData = db.read();
 
-  fs.readFile("db.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Ошибка чтения файла:", err);
-      return res
-        .status(500)
-        .json({ success: false, message: "Ошибка сервера при чтении файла" });
-    }
+  changes.forEach((change) => {
+    const [prefix, element, index, key] = change.id.split("-");
+    const text = change.text;
 
-    let jsonData;
-    try {
-      jsonData = JSON.parse(data);
-      console.log("Содержимое main_wabpage[0]:", jsonData.main_wabpage);
-
-      console.log("Содержимое main_wabpage[0]:", jsonData.main_wabpage[0]);
-    } catch (parseErr) {
-      console.error("Ошибка парсинга JSON:", parseErr);
-      return res
-        .status(500)
-        .json({ success: false, message: "Ошибка сервера при парсинге JSON" });
-    }
-
-    let updated = false;
-
-    // Функция для обновления текста в объекте
-    function updateText(obj, key, newText) {
-      if (obj && obj.hasOwnProperty(key)) {
-        console.log(`Обновление ключа ${key} значением ${newText}`);
-        obj[key] = newText;
-        updated = true;
+    if (dbData[dompage] && dbData[dompage][0][element]) {
+      if (Array.isArray(dbData[dompage][0][element])) {
+        dbData[dompage][0][element][index][key] = text;
       } else {
-        console.log(`Ключ ${key} не найден в объекте`);
+        dbData[dompage][0][element][key] = text;
       }
-    }
-
-    changes.forEach((change) => {
-      const [prefix, element, num, id, key] = change.id.split("-");
-      console.log("num :", num, "id :", id, "  key :", key);
-
-      const numericId = parseInt(id, 10) - 1; // Преобразование id в числовой индекс
-
-      console.log(`Обработка изменения: ${change.id} -> ${change.text}`);
-      switch (change.id) {
-        case "nun-element-1-banner-id":
-        case "nun-element-1-banner-title":
-          console.log(
-            "Содержимое jsonData.main_wabpage[0].banner:",
-            jsonData.main_wabpage[0].banner
-          );
-          if (
-            jsonData.main_wabpage[0].banner &&
-            jsonData.main_wabpage[0].banner.hasOwnProperty(key)
-          ) {
-            updateText(jsonData.main_wabpage[0].banner, key, change.text);
-          } else {
-            console.log(
-              `Ключ ${key} не найден в объекте main_wabpage[0].banner`
-            );
-          }
-          break;
-        case "nun-element-1-banner_mobile-id":
-        case "nun-element-1-banner_mobile-title":
-          console.log(
-            "Содержимое jsonData.main_wabpage[0].banner_mobile:",
-            jsonData.main_wabpage[0].banner_mobile
-          );
-          if (
-            jsonData.main_wabpage[0].banner_mobile &&
-            jsonData.main_wabpage[0].banner_mobile.hasOwnProperty(key)
-          ) {
-            updateText(
-              jsonData.main_wabpage[0].banner_mobile,
-              key,
-              change.text
-            );
-          } else {
-            console.log(
-              `Ключ ${key} не найден в объекте main_wabpage[0].banner_mobile`
-            );
-          }
-          break;
-        case "nun-element-1-raduga_info-id":
-        case "nun-element-1-raduga_info-title":
-        case "nun-element-1-raduga_info-text":
-          console.log(
-            "Содержимое jsonData.main_wabpage[0].raduga_info:",
-            jsonData.main_wabpage[0].raduga_info
-          );
-          if (
-            jsonData.main_wabpage[0].raduga_info &&
-            jsonData.main_wabpage[0].raduga_info.hasOwnProperty(key)
-          ) {
-            updateText(jsonData.main_wabpage[0].raduga_info, key, change.text);
-          } else {
-            console.log(
-              `Ключ ${key} не найден в объекте main_wabpage[0].raduga_info`
-            );
-          }
-          break;
-        case "nun-element-1-raduga_director-fio_director":
-        case "nun-element-1-raduga_director-additional_information":
-          console.log(
-            "Содержимое jsonData.main_wabpage[0].raduga_director:",
-            jsonData.main_wabpage[0].raduga_director
-          );
-          if (
-            jsonData.main_wabpage[0].raduga_director &&
-            jsonData.main_wabpage[0].raduga_director.hasOwnProperty(key)
-          ) {
-            updateText(
-              jsonData.main_wabpage[0].raduga_director,
-              key,
-              change.text
-            );
-          } else {
-            console.log(
-              `Ключ ${key} не найден в объекте main_wabpage[0].raduga_director`
-            );
-          }
-          break;
-        default:
-          console.error(`Неизвестный элемент: ${change.id}`);
-      }
-    });
-
-    if (updated) {
-      db.write(jsonData);
-      console.log("Текст успешно обновлен");
-      res.json({ success: true, message: "Текст обновлен" });
-    } else {
-      console.error("Текст не найден для обновления");
-      res.status(400).json({ success: false, message: "Текст не найден" });
     }
   });
+  const logToFile = (dbData) => fs.appendFileSync("output.log", `${dbData}\n`);
+  console.log = logToFile;
+
+  console.log("Сохранено в файл.");
+
+  // db.write(dbData);
+
+  res.send("Текст обновлен успешно!");
 });
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
